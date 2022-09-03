bfffed9bb0658b88f43f3d5784880056530508a0
eungyeol0506 <janess1@tukorea.ac.kr>
2022-09-03 23:50:51 +0900

    2022-09-03 Modify boardmapper

---
+++ 02-backend/simple-community/src/main/java/com/spring/board/common/Attach.java
@@ -0,0 +1,9 @@
+package com.spring.board.common;
+
+public class Attach {
+
+	int no;
+	String fileName;
+	int size;
+	Board board;
+}
--- 02-backend/simple-community/src/main/java/com/spring/board/persistence/IBoardMapper.java
+++ 02-backend/simple-community/src/main/java/com/spring/board/persistence/IBoardMapper.java
@@ -2,11 +2,13 @@ package com.spring.board.persistence;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.One;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
 import org.apache.ibatis.annotations.Update;
 
 import com.spring.board.common.Board;
@@ -39,8 +41,15 @@ public interface IBoardMapper {
 	})
 	Board getBoardByNo(int no);
 	
-	//조회수 증가 
+	//조회수 증가--------------------------------------------------------------------------------------------------------------
 	@Update("update board set views=views+1 where no=#{no}")
 	void incViews(int no);
 	
+	//게시판 작성--------------------------------------------------------------------------------------------------------------
+	@Insert("insert into `simple-community`.`board`(`title`,`contents`,`user_no`) values(#{title},#{contents},#{user_no})")
+	@SelectKey(statement="select last_insert_id()", keyProperty="no", before=false, resultType=int.class)
+	void createBoard(Board newboard);
+	
+	//게시판 수정
+	//게시판 삭제
 }
